53603175414b4b9c8f589d4ed5e7b63f061f4b20
GitHub Action <action@github.com>
2019-11-27 17:55:21 +0000

    Updates

---
+++ precache-manifest.e3f95bdc0d234f3b3dc344aa966f7a3f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3c489015d478f2485f63cdfbc9ed3f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a67547994f859c044e6d.bundle.js"
+  },
+  {
+    "url": "runtime~main.a67547994f859c044e6d.bundle.js"
+  },
+  {
+    "url": "vendors~main.a67547994f859c044e6d.bundle.js"
+  },
+  {
+    "url": "vendors~main.a67547994f859c044e6d.bundle.js.LICENSE"
+  }
+]);

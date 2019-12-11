b80805d349ba16419ec0363c43dfdebd31035395
Santiago Fraire Willemoës <santiago.fraire@kpn.com>
2019-12-11 12:50:47 +0100

    fix: boolean is passed properly to SideBarLink

--- packages/cra-template-kpn-style-react/template/src/components/router/SideBarLinkActive.js
+++ packages/cra-template-kpn-style-react/template/src/components/router/SideBarLinkActive.js
@@ -6,7 +6,7 @@ import { SideBarLink } from "@kpn-style/react";
 const SideBarLinkActive = ({ to, children }) => (
   <Match path={to}>
     {({ match }) => (
-      <SideBarLink tag={Link} to={to} active={match}>
+      <SideBarLink tag={Link} to={to} active={!!match}>
         {children}
       </SideBarLink>
     )}
